8b60b57ca8960dd71df1ea3c732e84d501b1ec31
HenryLudenaPyX <ludenahenry2@gmail.com>
2025-11-25 15:23:51 -0500

    Initial commit

---
+++ app/src/main/java/ludena/henry/proyectomovilesgrupo3/SessionFormActivity.kt
@@ -0,0 +1,27 @@
+package ludena.henry.proyectomovilesgrupo3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SessionFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_session_form)
+        val btnNext = findViewById<Button>(R.id.btnSiguiente)
+        btnNext.setOnClickListener {
+            val intent = Intent(this, ConfirmSessionActivity::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
