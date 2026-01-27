9b3e9021337c30acb72fb387f1e42cc0f47c250f
HenryLudenaPyX <ludenahenry2@gmail.com>
2026-01-26 23:17:18 -0500

    Add Firebase authentication to LoginActivity
    - Integrate Firebase Auth SDK and Google Services plugin.
    - Implement user authentication logic in `LoginActivity` using `signInWithEmailAndPassword`.
    - Add form validation for email and password fields, including required checks and minimum length requirements.
    - Update `AndroidManifest.xml` to set `LoginActivity` as the launcher activity.
    - Define new string resources for authentication error messages.
    - Create a `Constants.kt` file for intent extra keys.
    - Navigate to `TutorListActivity` upon successful login.

---
+++ app/src/main/java/ludena/henry/proyectomovilesgrupo3/Constants.kt
@@ -0,0 +1,3 @@
+package ludena.henry.proyectomovilesgrupo3
+
+const val EXTRA_LOGIN = "EXTRA_LOGIN"
--- app/src/main/java/ludena/henry/proyectomovilesgrupo3/LoginActivity.kt
+++ app/src/main/java/ludena/henry/proyectomovilesgrupo3/LoginActivity.kt
@@ -1,20 +1,90 @@
 package ludena.henry.proyectomovilesgrupo3
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var editTextEmail: EditText
+    lateinit var editTextPassword:EditText
+    lateinit var buttonLogin: Button
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        //Inicializar variables
+        buttonLogin = findViewById(R.id.btnLogin)
+        editTextEmail = findViewById(R.id.edtEmail)
+        editTextPassword = findViewById(R.id.edtPassword)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        //Eventos clic
+        buttonLogin.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            val clave = editTextPassword.text.toString()
+            //Validaciones de datos requeridos y formatos
+            if(!validateRequiredData())
+                return@setOnClickListener
+            AutenticarUsuario(email, clave)
         }
+
     }
+
+
+    fun AutenticarUsuario(email:String, password:String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(EXTRA_LOGIN, "signInWithEmail:success")
+                    //Si pasa validación de datos requeridos, ir a pantalla principal
+                    val intencion = Intent(this, TutorListActivity::class.java)
+                    intencion.putExtra(EXTRA_LOGIN, auth.currentUser!!.email)
+                    startActivity(intencion)
+                    //finish()
+                } else {
+                    Log.w(EXTRA_LOGIN, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, task.exception!!.message,
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun validateRequiredData():Boolean{
+        val email = editTextEmail.text.toString()
+        val password = editTextPassword.text.toString()
+        if (email.isEmpty()) {
+            editTextEmail.setError(getString(R.string.error_email_required))
+            editTextEmail.requestFocus()
+            return false
+        }
+        if (password.isEmpty()) {
+            editTextPassword.setError(getString(R.string.error_password_required))
+            editTextPassword.requestFocus()
+            return false
+        }
+        if (password.length < 3) {
+            editTextPassword.setError(getString(R.string.error_password_min_length))
+            editTextPassword.requestFocus()
+            return false
+        }
+        return true
+    }
+
+
 }
